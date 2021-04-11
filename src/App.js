b944807d5d49d337276196cbaf0edc472f26c434
DitwanP <price.ditwan@gmail.com>
2021-04-11 16:41:31 -0400

    Basic setup done, and created the hero component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import './styles/App.scss';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="index">
+        <Hero />
+      </div>
     </div>
   );
 }
