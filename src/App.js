7ed095dc5d077a70c4a33a1c7b4990528a21ef96
DitwanP <price.ditwan@gmail.com>
2021-04-19 16:22:12 -0400

    Added product header and div for the backing info.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './styles/App.scss';
 import Hero from './components/Hero';
+import Product from './components/Product';
+import BackingInfo from './components/BackingInfo';
 
 function App() {
   return (
     <div className="App">
       <div className="index">
         <Hero />
+        <Product />
+        <BackingInfo />
       </div>
     </div>
   );
---
+++ src/components/BackingInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/BackingInfo.scss';
+
+const BackingInfo = () => {
+    return (
+        <div className="backing-info">
+            
+        </div>
+    )
+}
+
+export default BackingInfo
